bbf415ef96af8c916487b96761c353649bd13053
SaraBarkat <ms_barkat@esi.dz>
2025-07-14 17:32:38 +0100

    update frontend

--- frontend/src/app/pages/product-details/product-details.component.ts
+++ frontend/src/app/pages/product-details/product-details.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true, 
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent {
 
+
+selectedTab: string = 'details';
+ChangeSelectedTab(tab: string) {
+  this.selectedTab = tab;
+  console.log('Selected tab:', this.selectedTab);
+}
 }
