06c7d4e4159e26500f89070c5d366f811dda9cdc
SaraBarkat <ms_barkat@esi.dz>
2025-07-14 14:12:47 +0100

    components add

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import {ProductCategoryComponent} from './pages/product-category/product-category.component';
+import {ProductDetailsComponent} from './pages/product-details/product-details.component';
+import {CartComponent}from './pages/cart/cart.component'  ;
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'Product/Category', component: ProductCategoryComponent },
+  { path: 'Product/Detail', component: ProductDetailsComponent },
+  { path: 'Cart', component: CartComponent },
+
+
+
+
 ];
